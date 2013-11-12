0960683a70340d634737415f41fc103602f89c25
Stian Lind Petlund <stianlp@gmail.com>
2013-11-12 17:41:58 +0100

    Added drawgraphs.js that draws graphs from analytics.js data

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
   res.sendfile(__dirname + '/views/index.html');
 });
 
+app.get('/testgraphs', function(req, res){
+  res.sendfile(__dirname + '/views/testgraphs.html');
+});
+
+
 app.get('/assets/:folder/:file', function(req, res){
   var folder = req.params.folder
     , file = req.params.file;
