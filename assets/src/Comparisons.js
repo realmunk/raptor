e7f2b148ba5aba58e0f2d0dcd2423201e558ea88
stianlp <lilleand3>
2013-11-27 15:30:47 +0100

    Changed Graphs.js to Comparisons.js Trends.js and Proportions.js

---
+++ assets/src/Comparisons.js
@@ -0,0 +1,19 @@
+(function (window, $, nv) {
+
+  window.RAPTOR = window.RAPTOR || {};
+  var ns = window.RAPTOR;
+
+  ns.Comparisons = function () {
+    var indicators = []
+
+    this.parseComparison = function parseComparison (indicators) {
+
+    } 
+
+    this.drawComparison = function drawComparison (indicator) { 
+      console.log("Drawing comparison");
+    };
+
+  };
+
+}(window, jQuery, nv));
---
+++ assets/src/Trends.js
@@ -0,0 +1,20 @@
+(function (window, $, nv) {
+
+  window.RAPTOR = window.RAPTOR || {};
+  var ns = window.RAPTOR;
+
+  ns.Trends = function () {
+    var indicators = []
+
+
+    this.parseComparison = function parseTrends (indicators) {
+      
+    } 
+
+    this.drawTrends = function drawTrends (indicators) { 
+      console.log("Drawing trends");
+    };
+
+  };
+
+}(window, jQuery, nv));
