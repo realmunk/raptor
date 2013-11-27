fead8c21bc73e13a65585ca2c648b692363aa8b5
stianlp <lilleand3>
2013-11-27 18:51:03 +0100

    Refactored drawComparisons and added rotated lables to histogram

--- assets/src/Comparisons.js
+++ assets/src/Comparisons.js
@@ -6,43 +6,29 @@
   ns.Comparisons = function () {
     var self = this;
 
-    this.parseComparison = function parseComparison (data, ids) {
-      
+    this.parseComparison = function parseComparison (data, ids) { 
       _.each(ids, function(id) {
         self.drawComparison(data, id);
       });
+    }
 
-    } 
+    self.drawComparison = function drawComparison (data, id) {
+      var rows = data.rows,
+          names = data.metaData.names,
+          plotData = [{
+          "key" : "some key",
+          "values" : []
+          }];
 
-    self.drawComparison = function drawComparison (data, id) { 
       $("#comparisons").append("<h2>" + data.metaData.names[id] + "<h2>");
       $("#comparisons").append('<div id=' + id + '><svg></svg></div>');
-      console.log("Drawing comparison");
-      drawHisto(data, id);
 
-    };
-
-    function drawHisto(data, id) {
-      var rows = data.rows,
-        plotData = {
-          "key" : "TestHisto", 
-          "values" : []
-        };
-
-      function getPoints() {
-        var i;
-        for (i = 0; i < rows.length; i += 1) {
-          console.log(id);
-          if (id === rows[i][0]) {
-            plotData.values.push({ "label" : rows[i][1],
-            "value" : parseFloat(rows[i][2])});
-        
-          }
+      _.each(rows, function(row) {
+        if (id === row[0]) {
+          plotData[0].values.push({ "label" : names[row[1]],
+          "value" : parseFloat(row[2])});
         }
-        return [plotData];
-      }
-
-      var points = getPoints();
+      });
 
       nv.addGraph(function () {
         var chart = nv.models.discreteBarChart()
@@ -50,17 +36,20 @@
           .y(function(d) { return d.value; })
           .showValues(true);
 
+        chart.xAxis.rotateLabels(-45);
+
         d3.select("#" + id + " svg")
-          .datum(points)
+          .datum(plotData)
           .transition().duration(500)
           .attr("width", 400)
-          .attr("height",100)
+          .attr("height",130)
           .call(chart);
         
         nv.utils.windowResize(chart.update);
         return chart;
       });
-    } 
+
+    };
 
   };
 
